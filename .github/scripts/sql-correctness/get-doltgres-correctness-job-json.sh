ee9c9c7cba0ecf3fce27c4a928eeef2cfbc7300a
coffeegoddd <dustin@dolthub.com>
2024-01-26 11:33:47 -0800

    /.github/{scripts,workflows}: add correctness workflows

---
+++ .github/scripts/sql-correctness/get-doltgres-correctness-job-json.sh
@@ -0,0 +1,114 @@
+#!/bin/sh
+
+set -e
+
+if [ "$#" -lt 5 ]; then
+    echo  "Usage: ./get-job-json.sh <jobname> <version> <timeprefix> <actorprefix> <format> <nomsBinFormat> <issueNumber> <regressComp> <branchRef>"
+    exit 1
+fi
+
+jobname="$1"
+version="$2"
+timeprefix="$3"
+actorprefix="$4"
+format="$5"
+nomsBinFormat="$6"
+issueNumber="$7"
+regressComp="$8"
+branchRef="$9"
+
+precision="6"
+
+if [ -n "$nomsBinFormat" ]; then
+  nomsBinFormat="\"--noms-bin-format=$nomsBinFormat\","
+fi
+
+if [ -n "$issueNumber" ]; then
+  issueNumber="\"--issue-number=$issueNumber\","
+fi
+
+regressPrec=""
+if [ -n "$regressComp" ]; then
+  regressComp="\"--regress-compare=$regressComp\","
+  regressPrec="\"--regress-precision=$precision\","
+  branchRef="\"--branch=$branchRef\","
+fi
+
+resultCountQuery="select version, result, count(*) as total from results where result != 'skipped' group by result;"
+testCountQuery="select version, count(*) as total_tests from results where result != 'skipped';"
+correctnessQuery="select ROUND(100.0 * (cast(ok_results.total as decimal) / (cast(all_results.total as decimal) + .000001)), $precision) as correctness_percentage from (select count(*) as total from results where result = 'ok') as ok_results join (select count(*) as total from results where result != 'skipped') as all_results"
+
+echo '
+{
+  "apiVersion": "batch/v1",
+  "kind": "Job",
+  "metadata": {
+    "name": "'$jobname'",
+    "namespace": "sql-correctness"
+  },
+  "spec": {
+    "backoffLimit": 3,
+    "template": {
+      "metadata": {
+        "annotations": {
+          "alert_recipients": "'$ACTOR_EMAIL'"
+        },
+        "labels": {
+          "k8s-liquidata-inc-monitored-job": "created-by-static-config"
+        }
+      },
+      "spec": {
+        "serviceAccountName": "sql-correctness",
+        "containers": [
+          {
+            "name": "sql-correctness",
+            "image": "407903926827.dkr.ecr.us-west-2.amazonaws.com/liquidata/sql-correctness:latest",
+            "resources": {
+              "limits": {
+                "cpu": "7000m"
+              }
+            },
+            "env": [
+              { "name": "REPO_ACCESS_TOKEN", "value": "'$REPO_ACCESS_TOKEN'"},
+              { "name": "ACTOR", "value": "'$ACTOR'"},
+              { "name": "ACTOR_EMAIL", "value": "'$ACTOR_EMAIL'"},
+              { "name": "DOLT_DEFAULT_BIN_FORMAT", "value": "'$NOMS_BIN_FORMAT'"}
+            ],
+            "args": [
+              "--schema=/correctness.sql",
+              "--concurrent",
+              "--output='$format'",
+              "--version='$version'",
+              "--timeout=600",
+              '"$nomsBinFormat"'
+              '"$issueNumber"'
+              '"$regressComp"'
+              '"$regressPrec"'
+              '"$branchRef"'
+              "--bucket=sql-correctness-github-actions-results",
+              "--region=us-west-2",
+              "--results-dir='$timeprefix'",
+              "--results-prefix='$actorprefix'",
+              "'"$resultCountQuery"'",
+              "'"$testCountQuery"'",
+              "'"$correctnessQuery"'"
+            ]
+          }
+        ],
+        "restartPolicy": "Never",
+        "nodeSelector": {
+          "sql-correctness-worker": "true"
+        },
+        "tolerations": [
+          {
+              "effect": "NoSchedule",
+              "key": "dedicated",
+              "operator": "Equal",
+              "value": "sql-correctness-worker"
+          }
+        ]
+      }
+    }
+  }
+}
+'
---
+++ .github/scripts/sql-correctness/run-correctness.sh
@@ -0,0 +1,106 @@
+#!/bin/bash
+
+set -e
+
+if [ -z "$KUBECONFIG" ]; then
+    echo  "Must set KUBECONFIG"
+    exit 1
+fi
+
+if [ -z "$TEMPLATE_SCRIPT" ]; then
+    echo  "Must set TEMPLATE_SCRIPT"
+    exit 1
+fi
+
+if [ -z "$NOMS_BIN_FORMAT" ]; then
+    echo  "Must set NOMS_BIN_FORMAT"
+    exit 1
+fi
+
+if [ -z "$VERSION" ]; then
+    echo  "Must set VERSION for correctness run"
+    exit 1
+fi
+
+if [ -z "$ACTOR" ]; then
+    echo  "Must set ACTOR"
+    exit 1
+fi
+
+if [ -z "$MODE" ]; then
+    echo  "Must set MODE"
+    exit 1
+fi
+
+if [ -n "$PR_NUMBER" ]; then
+  if [ -z "$REGRESS_COMP" ]; then
+    echo "Must set REGRESS_COMP for PR correctness comparisons"
+    exit 1
+  fi
+  if [ -z "$PR_BRANCH_REF" ]; then
+    echo "Must set PR_BRANCH_REF for PR correctness comparisons"
+    exit 1
+  fi
+fi
+
+# use first 8 characters of VERSION to differentiate
+# jobs
+short=${VERSION:0:8}
+lowered=$(echo "$ACTOR" | tr '[:upper:]' '[:lower:]')
+actorShort="$lowered-$short"
+
+# random sleep
+sleep 0.$[ ( $RANDOM % 10 )  + 1 ]s
+
+timesuffix=`date +%s%N`
+
+jobname=""
+if [ -n "$PR_NUMBER" ]; then
+  jobname="$lowered-$PR_NUMBER"
+else
+  jobname="$actorShort-$timesuffix"
+fi
+
+timeprefix=$(date +%Y/%m/%d)
+
+actorprefix="$MODE/$lowered/$jobname/$NOMS_BIN_FORMAT"
+
+format="markdown"
+if [[ "$MODE" = "release" || "$MODE" = "nightly" ]]; then
+  format="html"
+fi
+
+# set value to PR_NUMBER environment variable
+# or default to -1
+issuenumber=${PR_NUMBER:-"-1"}
+
+source \
+  "$TEMPLATE_SCRIPT" \
+  "$jobname" \
+  "$VERSION" \
+  "$timeprefix" \
+  "$actorprefix" \
+  "$format" \
+  "$NOMS_BIN_FORMAT" \
+  "$issuenumber" \
+  "$REGRESS_COMP" \
+  "$PR_BRANCH_REF" > job.json
+
+# delete existing job with same name if this is a pr job
+if [ -n "$PR_NUMBER" ]; then
+  out=$(KUBECONFIG="$KUBECONFIG" kubectl delete job/"$jobname" -n sql-correctness || true)
+  echo "Delete pr job if exists: $out"
+  sleep 45
+fi
+
+out=$(KUBECONFIG="$KUBECONFIG" kubectl apply -f job.json || true)
+
+if [ "$out" != "job.batch/$jobname created" ]; then
+  echo "something went wrong creating job... this job likely already exists in the cluster"
+  echo "$out"
+  exit 1
+else
+  echo "$out"
+fi
+
+exit 0
