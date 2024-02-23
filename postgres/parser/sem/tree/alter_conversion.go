29d46af0bf477be31471e2d9561ecbba25ecaf26
jennifersp <jennifer@dolthub.com>
2024-02-22 16:55:47 -0800

    rm redundant header from newly added files

--- postgres/parser/sem/tree/alter_conversion.go
+++ postgres/parser/sem/tree/alter_conversion.go
@@ -12,16 +12,6 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Copyright 2020 The Cockroach Authors.
-//
-// Use of this software is governed by the Business Source License
-// included in the file licenses/BSL.txt.
-//
-// As of the Change Date specified in that file, in accordance with
-// the Business Source License, use of this software will be governed
-// by the Apache License, Version 2.0, included in the file
-// licenses/APL.txt.
-
 package tree
 
 var _ Statement = &AlterConversion{}
