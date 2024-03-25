d79a0aad39b3283c0499472b28fadaf0f2abc6c2
jennifersp <jennifer@dolthub.com>
2024-03-25 16:51:56 -0700

    rm

--- postgres/parser/sem/tree/alter_type.go
+++ postgres/parser/sem/tree/alter_type.go
@@ -26,21 +26,6 @@ package tree
 
 import "github.com/dolthub/doltgresql/postgres/parser/lex"
 
-//ALTER TYPE name OWNER TO { new_owner | CURRENT_ROLE | CURRENT_USER | SESSION_USER }
-//ALTER TYPE name RENAME TO new_name
-//ALTER TYPE name SET SCHEMA new_schema
-//ALTER TYPE name RENAME ATTRIBUTE attribute_name TO new_attribute_name [ CASCADE | RESTRICT ]
-//ALTER TYPE name action [, ... ]
-//ALTER TYPE name ADD VALUE [ IF NOT EXISTS ] new_enum_value [ { BEFORE | AFTER } neighbor_enum_value ]
-//ALTER TYPE name RENAME VALUE existing_enum_value TO new_enum_value
-//ALTER TYPE name SET ( property = value [, ... ] )
-//
-//where action is one of:
-//
-//ADD ATTRIBUTE attribute_name data_type [ COLLATE collation ] [ CASCADE | RESTRICT ]
-//DROP ATTRIBUTE [ IF EXISTS ] attribute_name [ CASCADE | RESTRICT ]
-//ALTER ATTRIBUTE attribute_name [ SET DATA ] TYPE data_type [ COLLATE collation ] [ CASCADE | RESTRICT ]
-
 var _ Statement = &AlterType{}
 
 // AlterType represents an ALTER TYPE statement.
