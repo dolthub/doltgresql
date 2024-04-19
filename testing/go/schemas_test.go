609e08c7572da3ec9864287dce61172938588242
Zach Musgrave <zach@dolthub.com>
2024-04-18 17:09:26 -0700

    More tests

--- testing/go/schemas_test.go
+++ testing/go/schemas_test.go
@@ -72,11 +72,11 @@ var SchemaTests = []ScriptTest{
 				Query: "CREATE TABLE mySchema.test (pk BIGINT PRIMARY KEY, v1 BIGINT);",
 			},
 			{
-				Query:    "insert into mySchema.test values (1,1), (2,2)",
-				Expected: []sql.Row{},
+				Query: "CREATE TABLE otherSchema.test (pk BIGINT PRIMARY KEY, v1 BIGINT);",
 			},
 			{
-				Query: "CREATE TABLE otherSchema.test (pk BIGINT PRIMARY KEY, v1 BIGINT);",
+				Query:    "insert into mySchema.test values (1,1), (2,2)",
+				Expected: []sql.Row{},
 			},
 			{
 				Query:    "insert into otherSchema.test values (3,3), (4,4)",
@@ -92,9 +92,90 @@ var SchemaTests = []ScriptTest{
 			{
 				Query: "SELECT * FROM otherSchema.test;",
 				Expected: []sql.Row{
+					{3, 3},
+					{4, 4},
+				},
+			},
+		},
+	},
+	{
+		Name: "insert, update, delete, alter table with schema",
+		Assertions: []ScriptTestAssertion{
+			{
+				Query:    "create schema mySchema",
+			},
+			{
+				Query:    "create schema otherSchema",
+			},
+			{
+				Query: "CREATE TABLE mySchema.test (pk BIGINT PRIMARY KEY, v1 BIGINT);",
+			},
+			{
+				Query: "CREATE TABLE otherSchema.test (pk BIGINT PRIMARY KEY, v1 BIGINT);",
+			},
+			{
+				Query:    "insert into mySchema.test values (1,1), (2,2)",
+			},
+			{
+				Query:    "insert into otherSchema.test values (3,3), (4,4)",
+				Expected: []sql.Row{},
+			},
+			{
+				Query:    "update mySchema.test set v1 = 3 where pk = 1",
+			},
+			{
+				Query:    "update otherSchema.test set v1 = 4 where pk = 3",
+			},
+			{
+				Query:    "delete from mySchema.test where pk = 2",
+			},
+			{
+				Query:    "delete from otherSchema.test where pk = 4",
+			},
+			{
+				Query: "SELECT * FROM mySchema.test;",
+				Expected: []sql.Row{
+					{1, 3},
+				},
+			},
+			{
+				Query: "SELECT * FROM otherSchema.test;",
+				Expected: []sql.Row{
+					{3, 4},
+				},
+			},
+		},
+	},
+	{
+		Name: "schema does not exist",
+		Assertions: []ScriptTestAssertion{
+			{
+				Query:    "create schema mySchema",
+			},
+			{
+				Query: "CREATE TABLE mySchema.test (pk BIGINT PRIMARY KEY, v1 BIGINT);",
+			},
+			{
+				Query: "CREATE TABLE otherSchema.test (pk BIGINT PRIMARY KEY, v1 BIGINT);",
+				ExpectedErr: true,
+			},
+			{
+				Query:    "insert into mySchema.test values (1,1), (2,2)",
+			},
+			{
+				Query:    "insert into otherSchema.test values (3,3), (4,4)",
+				ExpectedErr: true,
+			},
+			{
+				Query: "SELECT * FROM mySchema.test;",
+				Expected: []sql.Row{
 					{1, 1},
 					{2, 2},
 				},
+			},
+			{
+				Query: "SELECT * FROM otherSchema.test;",
+				ExpectedErr: true,
 			},
 		},
 	},
