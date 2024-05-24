9777c28203fa394273c3053f146a3bb4fb82ed2c
Zach Musgrave <zach@dolthub.com>
2024-05-23 17:33:09 -0700

    Package name change

--- server/server.go
+++ server/server.go
@@ -28,7 +28,7 @@ import (
 	doltservercfg "github.com/dolthub/dolt/go/libraries/doltcore/servercfg"
 	"github.com/dolthub/dolt/go/libraries/doltcore/sqle/dfunctions"
 	"github.com/dolthub/dolt/go/libraries/doltcore/sqle/dsess"
-	"github.com/dolthub/dolt/go/libraries/doltcore/sqle/search_path"
+	"github.com/dolthub/dolt/go/libraries/doltcore/sqle/resolve"
 	"github.com/dolthub/dolt/go/libraries/utils/argparser"
 	"github.com/dolthub/dolt/go/libraries/utils/config"
 	"github.com/dolthub/dolt/go/libraries/utils/filesys"
@@ -59,7 +59,7 @@ func init() {
 	server.DefaultProtocolListenerFunc = NewListener
 	sqlserver.ExternalDisableUsers = true
 	dfunctions.VersionString = Version
-	search_path.UseSearchPath = true
+	resolve.UseSearchPath = true
 }
 
 // RunOnDisk starts the server based on the given args, while also using the local disk as the backing store.
