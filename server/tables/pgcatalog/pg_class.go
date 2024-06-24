fc1dd046fb24938a68973feecc1894718148f604
Taylor Bantle <taylor@dolthub.com>
2024-06-24 14:45:17 -0700

    Add partial pg_class and pg_attribute tables

--- server/tables/pgcatalog/pg_class.go
+++ server/tables/pgcatalog/pg_class.go
@@ -17,6 +17,8 @@ package pgcatalog
 import (
 	"io"
 
+	"github.com/dolthub/dolt/go/libraries/doltcore/sqle/dsess"
+	sqle "github.com/dolthub/go-mysql-server"
 	"github.com/dolthub/go-mysql-server/sql"
 
 	"github.com/dolthub/doltgresql/server/tables"
@@ -43,8 +45,56 @@ func (p PgClassHandler) Name() string {
 
 // RowIter implements the interface tables.Handler.
 func (p PgClassHandler) RowIter(ctx *sql.Context) (sql.RowIter, error) {
-	// TODO: Implement pg_class row iter
-	return emptyRowIter()
+	doltSession := dsess.DSessFromSess(ctx.Session)
+	c := sqle.NewDefault(doltSession.Provider()).Analyzer.Catalog
+
+	var class []Class
+
+	for _, db := range c.AllDatabases(ctx) {
+		// Get tables and table indexes
+		err := sql.DBTableIter(ctx, db, func(t sql.Table) (cont bool, err error) {
+			hasIndexes := false
+
+			if it, ok := t.(sql.IndexAddressable); ok {
+				idxs, err := it.GetIndexes(ctx)
+				if err != nil {
+					return false, err
+				}
+				for _, idx := range idxs {
+					class = append(class, Class{name: idx.ID(), hasIndexes: false, kind: "i"})
+				}
+
+				if len(idxs) > 0 {
+					hasIndexes = true
+				}
+			}
+
+			class = append(class, Class{name: t.Name(), hasIndexes: hasIndexes, kind: "r"})
+
+			return true, nil
+		})
+		if err != nil {
+			return nil, err
+		}
+
+		// Get views
+		if vdb, ok := db.(sql.ViewDatabase); ok {
+			views, err := vdb.AllViews(ctx)
+			if err != nil {
+				return nil, err
+			}
+
+			for _, view := range views {
+				class = append(class, Class{name: view.Name, hasIndexes: false, kind: "v"})
+			}
+		}
+
+	}
+
+	return &pgClassRowIter{
+		class: class,
+		idx:   0,
+	}, nil
 }
 
 // Schema implements the interface tables.Handler.
@@ -92,15 +142,64 @@ var pgClassSchema = sql.Schema{
 	{Name: "relpartbound", Type: pgtypes.Text, Default: nil, Nullable: true, Source: PgClassName},    // TODO: type pg_node_tree, collation C
 }
 
+type Class struct {
+	name       string
+	hasIndexes bool
+	kind       string // r = ordinary table, i = index, S = sequence, t = TOAST table, v = view, m = materialized view, c = composite type, f = foreign table, p = partitioned table, I = partitioned index
+}
+
 // pgClassRowIter is the sql.RowIter for the pg_class table.
 type pgClassRowIter struct {
+	class []Class
+	idx   int
 }
 
 var _ sql.RowIter = (*pgClassRowIter)(nil)
 
 // Next implements the interface sql.RowIter.
 func (iter *pgClassRowIter) Next(ctx *sql.Context) (sql.Row, error) {
-	return nil, io.EOF
+	if iter.idx >= len(iter.class) {
+		return nil, io.EOF
+	}
+	iter.idx++
+	cl := iter.class[iter.idx-1]
+
+	// TODO: Fill in the rest of the pg_class columns
+	return sql.Row{
+		int32(iter.idx), // oid
+		cl.name,         // relname
+		int32(0),        // relnamespace
+		int32(0),        // reltype
+		int32(0),        // reloftype
+		int32(0),        // relowner
+		int32(0),        // relam
+		int32(0),        // relfilenode
+		int32(0),        // reltablespace
+		int32(0),        // relpages
+		float32(0),      // reltuples
+		int32(0),        // relallvisible
+		int32(0),        // reltoastrelid
+		cl.hasIndexes,   // relhasindex
+		false,           // relisshared
+		"p",             // relpersistence
+		cl.kind,         // relkind
+		int16(0),        // relnatts
+		int16(0),        // relchecks
+		false,           // relhasrules
+		false,           // relhastriggers
+		false,           // relhassubclass
+		false,           // relrowsecurity
+		false,           // relforcerowsecurity
+		true,            // relispopulated
+		"d",             // relreplident
+		false,           // relispartition
+		int32(0),        // relrewrite
+		int32(0),        // relfrozenxid
+		int32(0),        // relminmxid
+		nil,             // relacl
+		nil,             // reloptions
+		nil,             // relpartbound
+	}, nil
 }
 
 // Close implements the interface sql.RowIter.
