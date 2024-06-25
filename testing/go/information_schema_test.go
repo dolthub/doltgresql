088fd29fae273852bc6a93fd31441e7f38c75b41
jennifersp <jennifer@dolthub.com>
2024-06-25 15:49:18 -0700

    add test

--- testing/go/information_schema_test.go
+++ testing/go/information_schema_test.go
@@ -10,11 +10,24 @@ func TestInfoSchemaTables(t *testing.T) {
 	RunScripts(t, []ScriptTest{
 		{
 			Name: "information_schema.tables",
+			SetUpScript: []string{
+				"create table test_table (id int)",
+			},
 			Assertions: []ScriptTestAssertion{
 				{
-					Query: `SELECT table_catalog, table_schema, table_name FROM information_schema.tables;`,
+					Query: `SELECT table_schema FROM information_schema.tables group by table_schema order by table_schema;`,
 					Expected: []sql.Row{
-						{},
+						{"information_schema"}, {"pg_catalog"}, {"public"},
+					},
+				},
+				{
+					// TODO: all table_catalog values should be "doltgres"
+					Query: `SELECT table_catalog, table_schema FROM information_schema.tables group by table_catalog, table_schema order by table_catalog;`,
+					Expected: []sql.Row{
+						{"def", "information_schema"},
+						{"doltgres", "pg_catalog"},
+						{"postgres", "pg_catalog"},
+						{"postgres", "public"},
 					},
 				},
 				{
