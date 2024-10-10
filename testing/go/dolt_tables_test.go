6c721ea720e76cebd85ae36ca1b9725646015f22
Zach Musgrave <zach@dolthub.com>
2024-10-10 10:27:54 -0700

    Dirty hack for dolt_docs table

---
+++ testing/go/dolt_tables_test.go
@@ -0,0 +1,60 @@
+// Copyright 2024 Dolthub, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package _go
+
+import (
+	"testing"
+
+	"github.com/dolthub/go-mysql-server/sql"
+)
+
+func TestUserSpaceDoltTables(t *testing.T) {
+	RunScripts(t, []ScriptTest{
+		{
+			Name: "dolt docs",
+			SetUpScript: []string{
+				"INSERT INTO dolt_docs values ('README.md', 'testing')",
+			},
+			Assertions: []ScriptTestAssertion{
+				{
+					Query: `SELECT * FROM dolt_docs`,
+					Expected: []sql.Row{
+						{"README.md", "testing"},
+					},
+				},
+			},
+		},
+		{
+			Name: "dolt schemas",
+			SetUpScript: []string{
+				"create view myView as select 2 + 2",
+			},
+			Assertions: []ScriptTestAssertion{
+				{
+					Query: `SELECT * FROM dolt_schemas`,
+					Expected: []sql.Row{
+						{
+							"view",
+							"myview",
+							"create view myView as select 2 + 2",
+							"{\"CreatedAt\":0}",
+							"NO_ENGINE_SUBSTITUTION,ONLY_FULL_GROUP_BY,STRICT_TRANS_TABLES",
+						},
+					},
+				},
+			},
+		},
+	})
+}
