5af5be4e93c65393874afffe7cc4a921bd96b71c
James Cor <james@dolthub.com>
2024-11-07 15:29:05 -0800

    formatting ahhh

--- testing/go/values_statement_test.go
+++ testing/go/values_statement_test.go
@@ -1,3 +1,17 @@
+// Copyright 2024 Dolthub, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package _go
 
 import (
@@ -7,34 +21,36 @@ import (
 )
 
 func TestValuesStatement(t *testing.T) {
-	RunScripts(t, []ScriptTest{
-		{
-			Name: "basic values statements",
-			SetUpScript: []string{},
-			Assertions: []ScriptTestAssertion{
-				{
-					Query: `SELECT * FROM (VALUES (1), (2), (3)) sqa;`,
-					Expected: []sql.Row{
-						{1},
-						{2},
-						{3},
-					},
+	RunScripts(t, ValuesStatementTests)
+}
+
+var ValuesStatementTests = []ScriptTest{
+	{
+		Name:        "basic values statements",
+		SetUpScript: []string{},
+		Assertions: []ScriptTestAssertion{
+			{
+				Query: `SELECT * FROM (VALUES (1), (2), (3)) sqa;`,
+				Expected: []sql.Row{
+					{1},
+					{2},
+					{3},
 				},
-				{
-					Query: `SELECT * FROM (VALUES (1, 2), (3, 4)) sqa;`,
-					Expected: []sql.Row{
-						{1, 2},
-						{3, 4},
-					},
+			},
+			{
+				Query: `SELECT * FROM (VALUES (1, 2), (3, 4)) sqa;`,
+				Expected: []sql.Row{
+					{1, 2},
+					{3, 4},
 				},
-				{
-					Query: `SELECT i * 10, j * 100 FROM (VALUES (1, 2), (3, 4)) sqa(i, j);`,
-					Expected: []sql.Row{
-						{10, 200},
-						{30, 400},
-					},
+			},
+			{
+				Query: `SELECT i * 10, j * 100 FROM (VALUES (1, 2), (3, 4)) sqa(i, j);`,
+				Expected: []sql.Row{
+					{10, 200},
+					{30, 400},
 				},
 			},
 		},
-	})
+	},
 }
