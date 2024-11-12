57a069d19d0203f67f83dc4fab31b23e9b70e659
al-sabr <e.bekrek@yandex.com>
2024-11-13 00:06:33 +0100

    Discarding last change,

--- cmd/doltgres/main.go
+++ cmd/doltgres/main.go
@@ -22,7 +22,6 @@ import (
 	"os"
 	"os/signal"
 	"path/filepath"
-	"runtime"
 	"strconv"
 	"strings"
 	"syscall"
@@ -204,9 +203,7 @@ func main() {
 	restoreIO := cli.InitIO()
 	defer restoreIO()
 
-	if runtime.GOOS != "windows" {
-		warnIfMaxFilesTooLow()
-	}
+	warnIfMaxFilesTooLow()
 
 	fs := filesys.LocalFS
 	// dEnv will be reloaded at server start to point to the data dir on the server config
