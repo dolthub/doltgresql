c0ccbbdf9eb576e284de2f04ea00e120bf70e137
Jason Fulghum <jason@dolthub.com>
2024-11-14 16:40:59 -0800

    Adding support for pg_get_serial_sequence(text,text)

--- server/functions/init.go
+++ server/functions/init.go
@@ -133,6 +133,7 @@ func Init() {
 	initPgIsInRecovery()
 	initPgPostmasterStartTime()
 	initPgRelationSize()
+	initPgGetSerialSequence()
 	initPgStatGetNumscans()
 	initPgTableIsVisible()
 	initPgTableSize()
