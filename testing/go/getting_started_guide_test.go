ad0028d6086d31654231e6bc4bf2ab0e21c0f2e5
Taylor Bantle <taylor@dolthub.com>
2024-12-11 15:43:35 -0800

    Add skipped test for where clause bug

--- testing/go/getting_started_guide_test.go
+++ testing/go/getting_started_guide_test.go
@@ -99,6 +99,11 @@ func TestGettingStartedGuide(t *testing.T) {
 						{"Aaron", "Son", "Engineering"},
 					},
 				},
+				{
+					Skip:     true, // This returns no rows for some reason. See https://github.com/dolthub/doltgresql/issues/1063
+					Query:    "select * from employees_teams where employee_id='0' and team_id='1';",
+					Expected: []sql.Row{{1, 0}},
+				},
 
 				// Examine the Diff
 				{
