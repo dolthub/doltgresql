47b9994e652222d4cf49bacd42e72c3d3548044d
Zach Musgrave <zach@dolthub.com>
2024-12-30 14:53:54 -0800

    tests for loading from a file, broken for CSV right now

--- testing/go/copy_test.go
+++ testing/go/copy_test.go
@@ -15,12 +15,18 @@
 package _go
 
 import (
+	"fmt"
+	"path/filepath"
 	"testing"
 
 	"github.com/dolthub/go-mysql-server/sql"
+	"github.com/stretchr/testify/require"
 )
 
 func TestCopy(t *testing.T) {
+	absTestDataDir, err := filepath.Abs("testdata")
+	require.NoError(t, err)
+	
 	RunScripts(t, []ScriptTest{
 		{
 			Name: "tab delimited with header",
@@ -136,6 +142,54 @@ bar`, "baz"},
 				{
 					Query:             "COPY test_info FROM STDIN (FORMAT CSV, HEADER TRUE, DELIMITER '|');",
 					CopyFromStdInFile: "psv-load.sql",
+				},
+				{
+					Query: "SELECT * FROM test_info order by 1;",
+					Expected: []sql.Row{
+						{4, "string for 4", 1},
+						{5, "string for 5", 0},
+						{6, "string for 6", 0},
+					},
+				},
+			},
+		},
+		{
+			Name: "csv from file",
+			Skip: true, // hard-coded to use tab separated files right now
+			SetUpScript: []string{
+				"CREATE TABLE tbl1 (pk int primary key, c1 varchar(100), c2 varchar(250));",
+			},
+			Assertions: []ScriptTestAssertion{
+				{
+					Query: fmt.Sprintf("COPY tbl1 FROM '%s' (FORMAT CSV)", filepath.Join(absTestDataDir, "csv-load-basic-cases.sql")),
+					SkipResultsCheck: true,
+				},
+				{
+					Query: "select * from tbl1 where pk = 6 order by pk;",
+					Expected: []sql.Row{
+						{6, `foo
+\\.
+bar`, "baz"},
+					},
+				},
+				{
+					Query: "select * from tbl1 where pk = 9;",
+					Expected: []sql.Row{
+						{9, nil, "''"},
+					},
+				},
+			},
+		},
+		{
+			Name: "tab delimited with header from file",
+			SetUpScript: []string{
+				"CREATE TABLE test (pk int primary key);",
+				"INSERT INTO test VALUES (0), (1);",
+				"CREATE TABLE test_info (id int, info varchar(255), test_pk int, primary key(id), foreign key (test_pk) references test(pk));",
+			},
+			Assertions: []ScriptTestAssertion{
+				{
+					Query:             fmt.Sprintf("COPY test_info FROM '%s' WITH (HEADER)", filepath.Join(absTestDataDir, "tab-load-with-header.sql")),
 				},
 				{
 					Query: "SELECT * FROM test_info order by 1;",
