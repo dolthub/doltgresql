e5e087a375f3eaa7615f2e0fd9006a7f29f82863
Zach Musgrave <zach@dolthub.com>
2025-01-03 16:10:49 -0800

    Bug fix for committing changes

--- server/connection_handler.go
+++ server/connection_handler.go
@@ -810,15 +810,16 @@ func (h *ConnectionHandler) handleCopyDone(_ *pgproto3.CopyDone) (stop bool, end
 		return false, false, err
 	}
 
-	// If we aren't in an explicit/user managed transaction, we need to commit the transaction
-	if !sqlCtx.GetIgnoreAutoCommit() {
-		txSession, ok := sqlCtx.Session.(sql.TransactionSession)
-		if !ok {
-			return false, false, fmt.Errorf("session does not implement sql.TransactionSession")
-		}
-		if err = txSession.CommitTransaction(sqlCtx, txSession.GetTransaction()); err != nil {
-			return false, false, err
-		}
+	// TODO: rather than always committing the transaction here, we should respect whether a transaction was 
+	//  expliclitly started and not commit if not. In order to do that, we need to not always set 
+	//  ctx.GetIgnoreAutoCommit(), and instead conditionally *not* insert a transaction closing iterator during chunk
+	//  processing. We need a new query flag to effectively do the latter though. 
+	txSession, ok := sqlCtx.Session.(sql.TransactionSession)
+	if !ok {
+		return false, false, fmt.Errorf("session does not implement sql.TransactionSession")
+	}
+	if err = txSession.CommitTransaction(sqlCtx, txSession.GetTransaction()); err != nil {
+		return false, false, err
 	}
 
 	h.copyFromStdinState = nil
