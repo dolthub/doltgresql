cf3c3e35798cb37dba45ee3e97c6435e31e48d62
Zach Musgrave <zach@dolthub.com>
2025-01-29 17:49:12 -0800

    tests and full support for various substring

--- server/functions/substr.go
+++ server/functions/substr.go
@@ -28,7 +28,9 @@ import (
 // initSubstr registers the functions to the catalog.
 func initSubstr() {
 	framework.RegisterFunction(substr_text_int32)
+	framework.RegisterFunction(substring_text_int32)
 	framework.RegisterFunction(substr_text_int32_int32)
+	framework.RegisterFunction(substring_text_int32_int32)
 	framework.RegisterFunction(substring_text_text)
 }
 
@@ -38,18 +40,30 @@ var substr_text_int32 = framework.Function2{
 	Return:     pgtypes.Text,
 	Parameters: [2]*pgtypes.DoltgresType{pgtypes.Text, pgtypes.Int32},
 	Strict:     true,
-	Callable: func(ctx *sql.Context, _ [3]*pgtypes.DoltgresType, str any, start any) (any, error) {
-		runes := []rune(str.(string))
-		if start.(int32) < 1 {
-			start = int32(1)
-		}
-		// start is 1-indexed
-		start = start.(int32) - int32(1)
-		if int(start.(int32)) >= len(runes) {
-			return "", nil
-		}
-		return string(runes[start.(int32):]), nil
-	},
+	Callable:   substring_text_int32_fn,
+}
+
+// substring_text_int32 represents the PostgreSQL function of the same name, taking the same parameters.
+var substring_text_int32 = framework.Function2{
+	Name:       "substring",
+	Return:     pgtypes.Text,
+	Parameters: [2]*pgtypes.DoltgresType{pgtypes.Text, pgtypes.Int32},
+	Strict:     true,
+	Callable:   substring_text_int32_fn,
+}
+
+// substring_text_int32_fn is a helper function for substr_text_int32 and substring_text_int32.
+func substring_text_int32_fn(ctx *sql.Context, _ [3]*pgtypes.DoltgresType, str any, start any) (any, error) {
+	runes := []rune(str.(string))
+	if start.(int32) < 1 {
+		start = int32(1)
+	}
+	// start is 1-indexed
+	start = start.(int32) - int32(1)
+	if int(start.(int32)) >= len(runes) {
+		return "", nil
+	}
+	return string(runes[start.(int32):]), nil
 }
 
 // substr_text_int32_int32 represents the PostgreSQL function of the same name, taking the same parameters.
@@ -58,29 +72,41 @@ var substr_text_int32_int32 = framework.Function3{
 	Return:     pgtypes.Text,
 	Parameters: [3]*pgtypes.DoltgresType{pgtypes.Text, pgtypes.Int32, pgtypes.Int32},
 	Strict:     true,
-	Callable: func(ctx *sql.Context, _ [4]*pgtypes.DoltgresType, str any, startInt any, countInt any) (any, error) {
-		start := startInt.(int32)
-		count := countInt.(int32)
-		runes := []rune(str.(string))
-		if count < 0 {
-			return nil, errors.Errorf("negative substring length not allowed")
-		}
-		// start is 1-indexed
-		start--
-		if start < 0 {
-			count += start
-			start = 0
-		}
-		if count <= 0 {
-			return "", nil
-		}
-		if int(start) >= len(runes) {
-			return "", nil
-		} else if int64(start)+int64(count) > int64(len(runes)) {
-			return string(runes[start:]), nil
-		}
-		return string(runes[start : start+count]), nil
-	},
+	Callable:   substring_text_int32_int32_fn,
+}
+
+// substr_text_int32_int32 represents the PostgreSQL function of the same name, taking the same parameters.
+var substring_text_int32_int32 = framework.Function3{
+	Name:       "substring",
+	Return:     pgtypes.Text,
+	Parameters: [3]*pgtypes.DoltgresType{pgtypes.Text, pgtypes.Int32, pgtypes.Int32},
+	Strict:     true,
+	Callable:   substring_text_int32_int32_fn,
+}
+
+// substring_text_int32_int32_fn is a helper function for substr_text_int32_int32.
+func substring_text_int32_int32_fn(ctx *sql.Context, _ [4]*pgtypes.DoltgresType, str any, startInt any, countInt any) (any, error) {
+	start := startInt.(int32)
+	count := countInt.(int32)
+	runes := []rune(str.(string))
+	if count < 0 {
+		return nil, errors.Errorf("negative substring length not allowed")
+	}
+	// start is 1-indexed
+	start--
+	if start < 0 {
+		count += start
+		start = 0
+	}
+	if count <= 0 {
+		return "", nil
+	}
+	if int(start) >= len(runes) {
+		return "", nil
+	} else if int64(start)+int64(count) > int64(len(runes)) {
+		return string(runes[start:]), nil
+	}
+	return string(runes[start : start+count]), nil
 }
 
 // substr_text_text represents the PostgreSQL function of the same name, taking the same parameters.
@@ -95,13 +121,13 @@ var substring_text_text = framework.Function2{
 		if err != nil {
 			return nil, err
 		}
-		
+
 		match := re.Find([]byte(str.(string)))
-		
+
 		if match == nil {
 			return nil, nil
 		}
-		
+
 		return string(match), nil
 	},
 }
