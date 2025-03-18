d4aea0537806dda8c3d2396e6de513a6352dd2b6
Zach Musgrave <zach@dolthub.com>
2025-03-17 17:02:35 -0700

    Update testing/go/enginetest/doltgres_engine_test.go
    Co-authored-by: Jason Fulghum <jason.fulghum@gmail.com>

--- testing/go/enginetest/doltgres_engine_test.go
+++ testing/go/enginetest/doltgres_engine_test.go
@@ -168,7 +168,7 @@ func (dcv *doltCommitValidator) CommitHash(val interface{}) (bool, string) {
 
 // Convenience test for debugging a single query. Unskip and set to the desired query.
 func TestSingleScript(t *testing.T) {
-	//t.Skip()
+	t.Skip()
 
 	var scripts = []queries.ScriptTest{
 		{
