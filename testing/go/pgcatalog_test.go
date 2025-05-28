b64544d0c29bbfd3b2bc4b16855cde7b945bf713
Zach Musgrave <zach@dolthub.com>
2025-05-28 16:13:51 -0700

    Another bug fix

--- testing/go/pgcatalog_test.go
+++ testing/go/pgcatalog_test.go
@@ -3832,7 +3832,13 @@ func TestPgType(t *testing.T) {
 					},
 				},
 				{
-					Skip: true, // TODO: use regproc type instead of text type.
+					Query: `SELECT t1.oid, t1.typname as basetype, t2.typname as arraytype, t2.typsubscript
+					FROM   pg_type t1 LEFT JOIN pg_type t2 ON (t1.typarray = t2.oid)
+					WHERE  t1.typarray <> 0 AND (t2.oid IS NULL OR t2.typsubscript::regproc <> 'array_subscript_handler'::regproc);`,
+					Expected: []sql.Row{},
+				},
+				{
+					Skip: true, // TODO: ERROR: function internal_binary_operator_func_<>(text, regproc) does not exist
 					Query: `SELECT t1.oid, t1.typname as basetype, t2.typname as arraytype, t2.typsubscript
 					FROM   pg_type t1 LEFT JOIN pg_type t2 ON (t1.typarray = t2.oid)
 					WHERE  t1.typarray <> 0 AND (t2.oid IS NULL OR t2.typsubscript <> 'array_subscript_handler'::regproc);`,
