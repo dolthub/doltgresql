5260ea0d9bfa398a51cd2cf862ee26a9c3b08db5
jennifersp <jennifer@dolthub.com>
2025-08-15 11:46:17 -0700

    fix

--- testing/go/functions_test.go
+++ testing/go/functions_test.go
@@ -3561,7 +3561,7 @@ func TestSetReturningFunctions(t *testing.T) {
 					},
 					{
 						Query:       `select * from generate_series('2020-01-01 00:00'::timestamp, '2020-01-02 03:00'::timestamp, '0 hour'::interval);`,
-						ExpectedErr: `function: 'unknown_func' not found`,
+						ExpectedErr: `step size cannot equal zero`,
 					},
 					{
 						Skip:  true, // TODO: cannot cast unknown to interval, but this should work
