5741a8cc7788d1879936dd040747af98c97a9954
Daylon Wilkins <daylon@liquidata.co>
2025-08-28 08:38:01 -0700

    ALTER TABLE changes for migration scripts

--- server/node/create_sequence.go
+++ server/node/create_sequence.go
@@ -15,10 +15,12 @@
 package node
 
 import (
+	"math"
 	"strings"
 
 	"github.com/cockroachdb/errors"
 	"github.com/dolthub/dolt/go/libraries/doltcore/doltdb"
+	"github.com/dolthub/dolt/go/libraries/doltcore/sqle"
 	"github.com/dolthub/go-mysql-server/sql"
 	"github.com/dolthub/go-mysql-server/sql/plan"
 	vitess "github.com/dolthub/vitess/go/vt/sqlparser"
@@ -26,12 +28,16 @@ import (
 	"github.com/dolthub/doltgresql/core"
 	"github.com/dolthub/doltgresql/core/id"
 	"github.com/dolthub/doltgresql/core/sequences"
+	pgexprs "github.com/dolthub/doltgresql/server/expression"
+	"github.com/dolthub/doltgresql/server/functions/framework"
+	pgtypes "github.com/dolthub/doltgresql/server/types"
 )
 
 // CreateSequence handles the CREATE SEQUENCE statement, along with SERIAL type definitions.
 type CreateSequence struct {
 	schema      string
 	ifNotExists bool
+	fromAlter   bool
 	sequence    *sequences.Sequence
 }
 
@@ -39,10 +45,11 @@ var _ sql.ExecSourceRel = (*CreateSequence)(nil)
 var _ vitess.Injectable = (*CreateSequence)(nil)
 
 // NewCreateSequence returns a new *CreateSequence.
-func NewCreateSequence(ifNotExists bool, schema string, sequence *sequences.Sequence) *CreateSequence {
+func NewCreateSequence(ifNotExists bool, schema string, fromAlter bool, sequence *sequences.Sequence) *CreateSequence {
 	return &CreateSequence{
 		schema:      schema,
 		ifNotExists: ifNotExists,
+		fromAlter:   fromAlter,
 		sequence:    sequence,
 	}
 }
@@ -87,6 +94,9 @@ func (c *CreateSequence) RowIter(ctx *sql.Context, r sql.Row) (sql.RowIter, erro
 		return nil, errors.Errorf(`relation "%s" already exists`, c.sequence.Id)
 	}
 	// Check that the OWNED BY is valid, if it exists
+	var table sql.Table
+	var tableSch sql.Schema
+	var tableColumn *sql.Column
 	if c.sequence.OwnerTable.IsValid() {
 		// The table will only have its name set, so we need to fill in the schema as well
 		c.sequence.OwnerTable = id.NewTable(schema, c.sequence.OwnerTable.TableName())
@@ -100,27 +110,56 @@ func (c *CreateSequence) RowIter(ctx *sql.Context, r sql.Row) (sql.RowIter, erro
 			return nil, errors.Errorf(`sequence cannot be owned by relation "%s"`, c.sequence.OwnerTable.TableName())
 		}
 
-		table, err := core.GetDoltTableFromContext(ctx, doltdb.TableName{Name: c.sequence.OwnerTable.TableName(), Schema: schema})
+		table, err = core.GetSqlTableFromContext(ctx, "", doltdb.TableName{Name: c.sequence.OwnerTable.TableName(), Schema: schema})
 		if err != nil {
 			return nil, err
 		}
 		if table == nil {
 			return nil, errors.Errorf(`table "%s" cannot be found but says it exists`, c.sequence.OwnerTable.TableName())
 		}
-		tableSch, err := table.GetSchema(ctx)
-		if err != nil {
-			return nil, err
-		}
-		colFound := false
-		for _, col := range tableSch.GetAllCols().GetColumns() {
+		tableSch = table.Schema()
+		for _, col := range tableSch {
 			if col.Name == c.sequence.OwnerColumn {
-				colFound = true
+				tableColumn = col.Copy()
 				break
 			}
 		}
-		if !colFound {
+		if tableColumn == nil {
 			return nil, errors.Errorf(`column "%s" of relation "%s" does not exist`,
 				c.sequence.OwnerColumn, c.sequence.OwnerTable.TableName())
+		}
+		// If this is from an ALTER TABLE statement, then we have to adjust the type since we didn't have that information earlier
+		if c.fromAlter {
+			dgType, ok := tableColumn.Type.(*pgtypes.DoltgresType)
+			if !ok {
+				return nil, errors.Errorf(`column "%s" of relation "%s" has unexpected type: "%s"`,
+					c.sequence.OwnerColumn, c.sequence.OwnerTable.TableName(), tableColumn.Type.String())
+			}
+			switch dgType.ID {
+			case pgtypes.Int16.ID:
+				c.sequence.DataTypeID = pgtypes.Int16.ID
+				if c.sequence.Minimum < int64(math.MinInt16) {
+					c.sequence.Minimum = int64(math.MinInt16)
+				}
+				if c.sequence.Maximum > int64(math.MaxInt16) {
+					c.sequence.Maximum = int64(math.MaxInt16)
+				}
+			case pgtypes.Int32.ID:
+				c.sequence.DataTypeID = pgtypes.Int32.ID
+				if c.sequence.Minimum < int64(math.MinInt32) {
+					c.sequence.Minimum = int64(math.MinInt32)
+				}
+				if c.sequence.Maximum > int64(math.MaxInt32) {
+					c.sequence.Maximum = int64(math.MaxInt32)
+				}
+			case pgtypes.Int64.ID:
+				c.sequence.DataTypeID = pgtypes.Int64.ID
+				// Minimum and Maximum are already set to the Int64 values
+			default:
+				// Not sure what we should do if we encounter a non-integer type, so we'll error for now
+				return nil, errors.Errorf(`column "%s" of relation "%s" has an unsupported type: "%s"`,
+					c.sequence.OwnerColumn, c.sequence.OwnerTable.TableName(), tableColumn.Type.String())
+			}
 		}
 	}
 	// Create the sequence since we know it's completely valid
@@ -132,6 +171,41 @@ func (c *CreateSequence) RowIter(ctx *sql.Context, r sql.Row) (sql.RowIter, erro
 	}
 	if err = collection.CreateSequence(ctx, c.sequence); err != nil {
 		return nil, err
+	}
+	if c.fromAlter {
+		if tableColumn == nil {
+			// This check is to satisfy the linter
+			return nil, errors.New(`somehow unable to find the matching table column which should have failed a previous step`)
+		}
+		// TODO: What happens when we add a sequence to a column that already has a default value? We'll error until we find out
+		if tableColumn.Default != nil || tableColumn.Generated != nil {
+			return nil, errors.New(`cannot add a sequence to a column that already has a default value`)
+		}
+		alterableTable, ok := table.(*sqle.AlterableDoltTable)
+		if !ok {
+			return nil, errors.Errorf(`expected a Dolt table but received "%T"`, table)
+		}
+		// TODO: Do we need to convert to a TableName and then call String? Are we reliant on the specific way it's formatted?
+		//  This is how it's done in the analyzer for SERIAL types, so assuming it's for a good reason.
+		seqName := doltdb.TableName{Name: c.sequence.Id.SequenceName(), Schema: c.sequence.Id.SchemaName()}.String()
+		nextVal, foundFunc, err := framework.GetFunction("nextval", pgexprs.NewTextLiteral(seqName))
+		if err != nil {
+			return nil, err
+		}
+		if !foundFunc {
+			return nil, errors.Errorf(`function "nextval" could not be found`)
+		}
+		tableColumn.Default = &sql.ColumnDefaultValue{
+			Expr:          nextVal,
+			OutType:       tableColumn.Type.(*pgtypes.DoltgresType),
+			Literal:       false,
+			ReturnNil:     false,
+			Parenthesized: false,
+		}
+		err = alterableTable.ModifyColumn(ctx, tableColumn.Name, tableColumn, nil)
+		if err != nil {
+			return nil, err
+		}
 	}
 	return sql.RowsToRowIter(), nil
 }
