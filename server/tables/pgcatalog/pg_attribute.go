1af8e9373c24ea01250dba198fcc66f32be4f5c3
Zach Musgrave <zach@dolthub.com>
2025-09-10 09:40:08 -0700

    Initial index implementation for pg_attribute

--- server/tables/pgcatalog/pg_attribute.go
+++ server/tables/pgcatalog/pg_attribute.go
@@ -41,6 +41,7 @@ func InitPgAttribute() {
 type PgAttributeHandler struct{}
 
 var _ tables.Handler = PgAttributeHandler{}
+var _ tables.IndexedTableHandler = PgAttributeHandler{}
 
 // Name implements the interface tables.Handler.
 func (p PgAttributeHandler) Name() string {
@@ -49,81 +50,286 @@ func (p PgAttributeHandler) Name() string {
 
 // RowIter implements the interface tables.Handler.
 func (p PgAttributeHandler) RowIter(ctx *sql.Context, partition sql.Partition) (sql.RowIter, error) {
-	// Use cached data from this process if it exists
+	// Use cached data from this session if it exists
 	pgCatalogCache, err := getPgCatalogCache(ctx)
 	if err != nil {
 		return nil, err
 	}
 
-	if pgCatalogCache.attributeCols == nil {
-		var cols []*sql.Column
-		var tableOIDs []id.Id
-		var colIdxs []int
-
-		err := functions.IterateCurrentDatabase(ctx, functions.Callbacks{
-			Table: func(ctx *sql.Context, _ functions.ItemSchema, table functions.ItemTable) (cont bool, err error) {
-				for i, col := range table.Item.Schema() {
-					cols = append(cols, col)
-					colIdxs = append(colIdxs, i)
-					tableOIDs = append(tableOIDs, table.OID.AsId())
-				}
-				return true, nil
-			},
-		})
+	if pgCatalogCache.pgAttributes == nil {
+		err = cachePgAttributes(ctx, pgCatalogCache)
 		if err != nil {
 			return nil, err
 		}
-
-		if includeSystemTables {
-			_, root, err := core.GetRootFromContext(ctx)
-			if err != nil {
-				return nil, err
-			}
-
-			systemTables, err := resolve.GetGeneratedSystemTables(ctx, root)
-			if err != nil {
-				return nil, err
-			}
-
-			db := ctx.GetCurrentDatabase()
-			for _, tblName := range systemTables {
-				tbl, err := core.GetSqlTableFromContext(ctx, db, tblName)
-				if err != nil {
-					// Some of the system tables exist conditionally when accessed, so just skip them in this case
-					if errors.Is(doltdb.ErrTableNotFound, err) {
-						continue
-					}
-					return nil, err
-				}
-
-				schema := tbl.Schema()
-				for i, col := range schema {
-					cols = append(cols, col)
-					colIdxs = append(colIdxs, i)
-					tableOIDs = append(tableOIDs, id.NewTable(tblName.Schema, tblName.Name).AsId())
-				}
-			}
-		}
-
-		pgCatalogCache.attributeCols = cols
-		pgCatalogCache.attributeColIdxs = colIdxs
-		pgCatalogCache.attributeTableOIDs = tableOIDs
 	}
 
-	return &pgAttributeRowIter{
-		cols:      pgCatalogCache.attributeCols,
-		colIdxs:   pgCatalogCache.attributeColIdxs,
-		tableOIDs: pgCatalogCache.attributeTableOIDs,
-		idx:       0,
+	if attrIdxPart, ok := partition.(inMemIndexPartition); ok {
+		return &inMemIndexScanIter[*pgAttribute]{
+			lookup:         attrIdxPart.lookup,
+			rangeConverter: p,
+			btreeAccess:    pgCatalogCache.pgAttributes,
+			rowConverter:   pgAttributeToRow,
+			rangeIdx:       0,
+			nextChan:       nil,
+		}, nil
+	}
+
+	return &pgAttributeTableScanIter{
+		attributeCache: pgCatalogCache.pgAttributes,
+		idx:            0,
 	}, nil
 }
 
-// Schema implements the interface tables.Handler.
+// cachePgAttributes caches the pg_attribute data for the current database in the session.
+func cachePgAttributes(ctx *sql.Context, pgCatalogCache *pgCatalogCache) error {
+	var attributes []*pgAttribute
+	attrelidIdx := NewNonUniqueInMemIndexStorage[*pgAttribute](lessAttrelid)
+	attrelidAttnameIdx := NewUniqueInMemIndexStorage[*pgAttribute](lessAttrelidAttname)
+
+	err := functions.IterateCurrentDatabase(ctx, functions.Callbacks{
+		Table: func(ctx *sql.Context, _ functions.ItemSchema, table functions.ItemTable) (cont bool, err error) {
+			for i, col := range table.Item.Schema() {
+				typeOid := id.Null
+				if doltgresType, ok := col.Type.(*pgtypes.DoltgresType); ok {
+					typeOid = doltgresType.ID.AsId()
+				} else {
+					// TODO: Remove once all information_schema tables are converted to use DoltgresType
+					dt := pgtypes.FromGmsType(col.Type)
+					typeOid = dt.ID.AsId()
+				}
+
+				generated := ""
+				if col.Generated != nil {
+					generated = "s"
+				}
+
+				dimensions := int16(0)
+				if s, ok := col.Type.(sql.SetType); ok {
+					dimensions = int16(s.NumberOfElements())
+				}
+
+				hasDefault := col.Default != nil
+
+				attr := &pgAttribute{
+					attrelid:       table.OID.AsId(),
+					attrelidNative: id.Cache().ToOID(table.OID.AsId()),
+					attname:        col.Name,
+					atttypid:       typeOid,
+					attnum:         int16(i + 1),
+					attndims:       dimensions,
+					attnotnull:     !col.Nullable,
+					atthasdef:      hasDefault,
+					attgenerated:   generated,
+				}
+				attrelidIdx.Add(attr)
+				attrelidAttnameIdx.Add(attr)
+				attributes = append(attributes, attr)
+			}
+			return true, nil
+		},
+	})
+	if err != nil {
+		return err
+	}
+
+	if includeSystemTables {
+		_, root, err := core.GetRootFromContext(ctx)
+		if err != nil {
+			return err
+		}
+
+		systemTables, err := resolve.GetGeneratedSystemTables(ctx, root)
+		if err != nil {
+			return err
+		}
+
+		db := ctx.GetCurrentDatabase()
+		for _, tblName := range systemTables {
+			tbl, err := core.GetSqlTableFromContext(ctx, db, tblName)
+			if err != nil {
+				// Some of the system tables exist conditionally when accessed, so just skip them in this case
+				if errors.Is(doltdb.ErrTableNotFound, err) {
+					continue
+				}
+				return err
+			}
+
+			schema := tbl.Schema()
+			for i, col := range schema {
+				typeOid := id.Null
+				if doltgresType, ok := col.Type.(*pgtypes.DoltgresType); ok {
+					typeOid = doltgresType.ID.AsId()
+				} else {
+					dt := pgtypes.FromGmsType(col.Type)
+					typeOid = dt.ID.AsId()
+				}
+
+				generated := ""
+				if col.Generated != nil {
+					generated = "s"
+				}
+
+				dimensions := int16(0)
+				if s, ok := col.Type.(sql.SetType); ok {
+					dimensions = int16(s.NumberOfElements())
+				}
+
+				hasDefault := col.Default != nil
+
+				attr := &pgAttribute{
+					attrelid:       id.NewTable(tblName.Schema, tblName.Name).AsId(),
+					attrelidNative: id.Cache().ToOID(id.NewTable(tblName.Schema, tblName.Name).AsId()),
+					attname:        col.Name,
+					atttypid:       typeOid,
+					attnum:         int16(i + 1),
+					attndims:       dimensions,
+					attnotnull:     !col.Nullable,
+					atthasdef:      hasDefault,
+					attgenerated:   generated,
+				}
+				attrelidIdx.Add(attr)
+				attrelidAttnameIdx.Add(attr)
+				attributes = append(attributes, attr)
+			}
+		}
+	}
+
+	pgCatalogCache.pgAttributes = &pgAttributeCache{
+		attributes:         attributes,
+		attrelidIdx:        attrelidIdx,
+		attrelidAttnameIdx: attrelidAttnameIdx,
+	}
+
+	return nil
+}
+
+// getIndexScanRange implements the interface RangeConverter.
+func (p PgAttributeHandler) getIndexScanRange(rng sql.Range, index sql.Index) (*pgAttribute, bool, *pgAttribute, bool) {
+	var gte, lte *pgAttribute
+	var hasLowerBound, hasUpperBound bool
+
+	switch index.(pgCatalogInMemIndex).name {
+	case "pg_attribute_attrelid_index":
+		msrng := rng.(sql.MySQLRange)
+		oidRng := msrng[0]
+		if oidRng.HasLowerBound() {
+			lb := sql.GetMySQLRangeCutKey(oidRng.LowerBound)
+			if lb != nil {
+				lowerRangeCutKey := lb.(id.Id)
+				gte = &pgAttribute{
+					attrelidNative: idToOid(lowerRangeCutKey),
+				}
+				hasLowerBound = true
+			}
+		}
+
+		if oidRng.HasUpperBound() {
+			ub := sql.GetMySQLRangeCutKey(oidRng.UpperBound)
+			if ub != nil {
+				upperRangeCutKey := ub.(id.Id)
+				lte = &pgAttribute{
+					attrelidNative: idToOid(upperRangeCutKey),
+				}
+				hasUpperBound = true
+			}
+		}
+
+	case "pg_attribute_attrelid_attname_index":
+		msrng := rng.(sql.MySQLRange)
+		attrelidRange := msrng[0]
+		attnameRange := msrng[1]
+		var attrelidLower, attrelidUpper uint32
+		var attnameLower, attnameUpper string
+
+		if attrelidRange.HasLowerBound() {
+			lb := sql.GetMySQLRangeCutKey(attrelidRange.LowerBound)
+			if lb != nil {
+				lowerRangeCutKey := lb.(id.Id)
+				attrelidLower = idToOid(lowerRangeCutKey)
+				hasLowerBound = true
+			}
+		}
+		if attrelidRange.HasUpperBound() {
+			ub := sql.GetMySQLRangeCutKey(attrelidRange.UpperBound)
+			if ub != nil {
+				upperRangeCutKey := ub.(id.Id)
+				attrelidUpper = idToOid(upperRangeCutKey)
+				hasUpperBound = true
+			}
+		}
+
+		if attnameRange.HasLowerBound() {
+			lb := sql.GetMySQLRangeCutKey(attnameRange.LowerBound)
+			if lb != nil {
+				attnameLower = lb.(string)
+			}
+		}
+		if attnameRange.HasUpperBound() {
+			ub := sql.GetMySQLRangeCutKey(attnameRange.UpperBound)
+			if ub != nil {
+				attnameUpper = ub.(string)
+			}
+		}
+
+		if attrelidRange.HasLowerBound() || attnameRange.HasLowerBound() {
+			gte = &pgAttribute{
+				attrelidNative: attrelidLower,
+				attname:        attnameLower,
+			}
+		}
+
+		if attrelidRange.HasUpperBound() || attnameRange.HasUpperBound() {
+			lte = &pgAttribute{
+				attrelidNative: attrelidUpper,
+				attname:        attnameUpper,
+			}
+		}
+	default:
+		panic("unknown index name: " + index.(pgCatalogInMemIndex).name)
+	}
+
+	return gte, hasLowerBound, lte, hasUpperBound
+}
+
+// PkSchema implements the interface tables.Handler.
 func (p PgAttributeHandler) PkSchema() sql.PrimaryKeySchema {
 	return sql.PrimaryKeySchema{
 		Schema:     pgAttributeSchema,
 		PkOrdinals: nil,
 	}
+}
+
+// Indexes implements tables.IndexedTableHandler.
+func (p PgAttributeHandler) Indexes() ([]sql.Index, error) {
+	return []sql.Index{
+		pgCatalogInMemIndex{
+			name:        "pg_attribute_attrelid_index",
+			tblName:     "pg_attribute",
+			dbName:      "pg_catalog",
+			uniq:        false,
+			columnExprs: []sql.ColumnExpressionType{{Expression: "pg_attribute.attrelid", Type: pgtypes.Oid}},
+		},
+		pgCatalogInMemIndex{
+			name:    "pg_attribute_attrelid_attname_index",
+			tblName: "pg_attribute",
+			dbName:  "pg_catalog",
+			uniq:    true,
+			columnExprs: []sql.ColumnExpressionType{
+				{Expression: "pg_attribute.attrelid", Type: pgtypes.Oid},
+				{Expression: "pg_attribute.attname", Type: pgtypes.Name},
+			},
+		},
+	}, nil
+}
+
+// LookupPartitions implements tables.IndexedTableHandler.
+func (p PgAttributeHandler) LookupPartitions(context *sql.Context, lookup sql.IndexLookup) (sql.PartitionIter, error) {
+	return &inMemIndexPartIter{
+		part: inMemIndexPartition{
+			idxName: lookup.Index.(pgCatalogInMemIndex).name,
+			lookup:  lookup,
+		},
+	}, nil
 }
 
 // pgAttributeSchema is the schema for pg_attribute.
@@ -156,79 +362,85 @@ var pgAttributeSchema = sql.Schema{
 	{Name: "attmissingval", Type: pgtypes.AnyArray, Default: nil, Nullable: true, Source: PgAttributeName},
 }
 
-// pgAttributeRowIter is the sql.RowIter for the pg_attribute table.
-type pgAttributeRowIter struct {
-	cols      []*sql.Column
-	colIdxs   []int
-	tableOIDs []id.Id
-	idx       int
+// pgAttribute represents a row in the pg_attribute table.
+// We store oids in their native format as well so that we can do range scans on them.
+type pgAttribute struct {
+	attrelid       id.Id
+	attrelidNative uint32
+	attname        string
+	atttypid       id.Id
+	attnum         int16
+	attndims       int16
+	attnotnull     bool
+	atthasdef      bool
+	attgenerated   string
 }
 
-var _ sql.RowIter = (*pgAttributeRowIter)(nil)
+// lessAttrelid is a sort function for pgAttribute based on attrelid.
+func lessAttrelid(a, b []*pgAttribute) bool {
+	return a[0].attrelidNative < b[0].attrelidNative
+}
+
+// lessAttrelidAttname is a sort function for pgAttribute based on attrelid, then attname.
+func lessAttrelidAttname(a, b *pgAttribute) bool {
+	if a.attrelidNative == b.attrelidNative {
+		return a.attname < b.attname
+	}
+	return a.attrelidNative < b.attrelidNative
+}
+
+// pgAttributeTableScanIter is the sql.RowIter for the pg_attribute table.
+type pgAttributeTableScanIter struct {
+	attributeCache *pgAttributeCache
+	idx            int
+}
+
+var _ sql.RowIter = (*pgAttributeTableScanIter)(nil)
 
 // Next implements the interface sql.RowIter.
-func (iter *pgAttributeRowIter) Next(ctx *sql.Context) (sql.Row, error) {
-	if iter.idx >= len(iter.cols) {
+func (iter *pgAttributeTableScanIter) Next(ctx *sql.Context) (sql.Row, error) {
+	if iter.idx >= len(iter.attributeCache.attributes) {
 		return nil, io.EOF
 	}
 	iter.idx++
-	col := iter.cols[iter.idx-1]
-	tableOid := iter.tableOIDs[iter.idx-1]
-	colIdx := iter.colIdxs[iter.idx-1]
+	attr := iter.attributeCache.attributes[iter.idx-1]
 
-	generated := ""
-	if col.Generated != nil {
-		generated = "s"
-	}
-
-	dimensions := 0
-	if s, ok := col.Type.(sql.SetType); ok {
-		dimensions = int(s.NumberOfElements())
-	}
-
-	hasDefault := col.Default != nil
-
-	typeOid := id.Null
-	if doltgresType, ok := col.Type.(*pgtypes.DoltgresType); ok {
-		typeOid = doltgresType.ID.AsId()
-	} else {
-		// TODO: Remove once all information_schema tables are converted to use DoltgresType
-		dt := pgtypes.FromGmsType(col.Type)
-		typeOid = dt.ID.AsId()
-	}
-
-	// TODO: Fill in the rest of the pg_attribute columns
-	return sql.Row{
-		tableOid,          // attrelid
-		col.Name,          // attname
-		typeOid,           // atttypid
-		int16(0),          // attlen
-		int16(colIdx + 1), // attnum
-		int32(-1),         // attcacheoff
-		int32(-1),         // atttypmod
-		int16(dimensions), // attndims
-		false,             // attbyval
-		"i",               // attalign
-		"p",               // attstorage
-		"",                // attcompression
-		!col.Nullable,     // attnotnull
-		hasDefault,        // atthasdef
-		false,             // atthasmissing
-		"",                // attidentity
-		generated,         // attgenerated
-		false,             // attisdropped
-		true,              // attislocal
-		int16(0),          // attinhcount
-		int16(-1),         // attstattarget
-		id.Null,           // attcollation
-		nil,               // attacl
-		nil,               // attoptions
-		nil,               // attfdwoptions
-		nil,               // attmissingval
-	}, nil
+	return pgAttributeToRow(attr), nil
 }
 
 // Close implements the interface sql.RowIter.
-func (iter *pgAttributeRowIter) Close(ctx *sql.Context) error {
+func (iter *pgAttributeTableScanIter) Close(ctx *sql.Context) error {
 	return nil
+}
+
+func pgAttributeToRow(attr *pgAttribute) sql.Row {
+	// TODO: Fill in the rest of the pg_attribute columns
+	return sql.Row{
+		attr.attrelid,   // attrelid
+		attr.attname,    // attname
+		attr.atttypid,   // atttypid
+		int16(0),        // attlen
+		attr.attnum,     // attnum
+		int32(-1),       // attcacheoff
+		int32(-1),       // atttypmod
+		attr.attndims,   // attndims
+		false,           // attbyval
+		"i",             // attalign
+		"p",             // attstorage
+		"",              // attcompression
+		attr.attnotnull, // attnotnull
+		attr.atthasdef,  // atthasdef
+		false,           // atthasmissing
+		"",              // attidentity
+		attr.attgenerated, // attgenerated
+		false,           // attisdropped
+		true,            // attislocal
+		int16(0),        // attinhcount
+		int16(-1),       // attstattarget
+		id.Null,         // attcollation
+		nil,             // attacl
+		nil,             // attoptions
+		nil,             // attfdwoptions
+		nil,             // attmissingval
+	}
 }
--- server/tables/pgcatalog/pg_catalog_cache.go
+++ server/tables/pgcatalog/pg_catalog_cache.go
@@ -48,9 +48,7 @@ type pgCatalogCache struct {
 	schemaOids  []id.Id
 
 	// pg_attribute
-	attributeCols      []*sql.Column
-	attributeTableOIDs []id.Id
-	attributeColIdxs   []int
+	pgAttributes *pgAttributeCache
 
 	// pg_index / pg_indexes
 	pgIndexes *pgIndexCache
@@ -105,6 +103,26 @@ type pgIndexCache struct {
 }
 
 var _ BTreeStorageAccess[*pgIndex] = &pgIndexCache{}
+
+type pgAttributeCache struct {
+	attributes         []*pgAttribute
+	attrelidIdx        *inMemIndexStorage[*pgAttribute]
+	attrelidAttnameIdx *inMemIndexStorage[*pgAttribute]
+}
+
+var _ BTreeStorageAccess[*pgAttribute] = &pgAttributeCache{}
+
+// getIndex implements BTreeStorageAccess.
+func (p pgAttributeCache) getIndex(name string) *inMemIndexStorage[*pgAttribute] {
+	switch name {
+	case "pg_attribute_attrelid_index":
+		return p.attrelidIdx
+	case "pg_attribute_attrelid_attname_index":
+		return p.attrelidAttnameIdx
+	default:
+		panic("unknown pg_attribute index: " + name)
+	}
+}
 
 // getIndex implements BTreeStorageAccess.
 func (p pgIndexCache) getIndex(name string) *inMemIndexStorage[*pgIndex] {
