03cd5cd975634b55e4bde6b150a8b92454788146
Hydrocharged <Hydrocharged@users.noreply.github.com>
2026-02-11 12:41:46 +0000

    Update DoltgreSQL version to 0.55.0

--- server/server.go
+++ server/server.go
@@ -48,7 +48,7 @@ import (
 // Version should have a new line that follows, else the formatter will fail the PR created by the release GH action
 
 const (
-	Version = "0.54.10"
+	Version = "0.55.0"
 
 	DefUserName         = "postres"
 	DefUserEmail        = "postgres@somewhere.com"
