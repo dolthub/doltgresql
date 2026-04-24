cdaf39c9c933ddf7c5c9802f03f69095ff5d8764
Zach Musgrave <zach@dolthub.com>
2026-04-24 12:32:32 -0700

    formatting

--- server/types/type.go
+++ server/types/type.go
@@ -612,7 +612,7 @@ func (t *DoltgresType) IoOutput(ctx *sql.Context, val any) (string, error) {
 // Or it can be pseudo category with name 'anyarray'.
 func (t *DoltgresType) IsArrayType() bool {
 	return (t.TypCategory == TypeCategory_ArrayTypes && t.Elem != id.NullType && t.Array == id.NullType) ||
-			(t.TypCategory == TypeCategory_PseudoTypes && t.ID.TypeName() == "anyarray")
+		(t.TypCategory == TypeCategory_PseudoTypes && t.ID.TypeName() == "anyarray")
 }
 
 // IsArrayCategory returns true if the type is of 'array' category.
