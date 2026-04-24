298b82c77fd74bc575092963899aab6493c532c2
Zach Musgrave <zach@dolthub.com>
2026-04-24 13:20:13 -0700

    turn json encoding back on

--- server/types/typeinfo.go
+++ server/types/typeinfo.go
@@ -69,9 +69,8 @@ func (t typeInfo) Encoding() val.Encoding {
 		return val.DecimalEnc
 	case "bytea":
 		return val.BytesAdaptiveEnc
-	// TODO: use dolt JSON document encoding here
-	// case "json", "jsonb":
-	// 	return val.JSONAddrEnc
+	case "json", "jsonb":
+		return val.JSONAddrEnc
 	case "xid":
 		return val.Uint32Enc
 		// TODO: uuid is represented as a uuid.Uuid in doltgres, but dolt wants []byte for BytesAdaptiveEnc
