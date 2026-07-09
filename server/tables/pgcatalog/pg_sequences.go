f4064607db17c839fff2c2db88fdc310602c26bf
jennifersp <jennifer@dolthub.com>
2026-07-09 14:14:00 -0700

    fix

--- server/tables/pgcatalog/pg_sequences.go
+++ server/tables/pgcatalog/pg_sequences.go
@@ -75,7 +75,10 @@ var pgSequencesSchema = sql.Schema{
 	{Name: "sequencename", Type: pgtypes.Name, Default: nil, Nullable: true, Source: PgSequencesName},
 	{Name: "sequenceowner", Type: pgtypes.Name, Default: nil, Nullable: true, Source: PgSequencesName},
 	{Name: "data_type", Type: pgtypes.Regtype, Default: nil, Nullable: true, Source: PgSequencesName},
+<<<<<<< Updated upstream
 
+=======
+>>>>>>> Stashed changes
 	{Name: "start_value", Type: pgtypes.Int64, Default: nil, Nullable: true, Source: PgSequencesName},
 	{Name: "min_value", Type: pgtypes.Int64, Default: nil, Nullable: true, Source: PgSequencesName},
 	{Name: "max_value", Type: pgtypes.Int64, Default: nil, Nullable: true, Source: PgSequencesName},
@@ -112,17 +115,17 @@ func (iter *pgSequencesRowIter) Next(_ *sql.Context) (sql.Row, error) {
 	}
 
 	return sql.Row{
-		schemaName,                     // schemaname
-		sequence.Id.SequenceName(),     // sequencename
-		nil,                            // sequenceowner
-		sequence.DataTypeID.TypeName(), // data_type
-		sequence.Start,                 // start_value
-		sequence.Minimum,               // min_value
-		sequence.Maximum,               // max_value
-		sequence.Increment,             // increment_by
-		sequence.Cycle,                 // cycle
-		sequence.Cache,                 // cache_size
-		lastValue,                      // last_value
+		schemaName,                 // schemaname
+		sequence.Id.SequenceName(), // sequencename
+		nil,                        // sequenceowner
+		sequence.DataTypeID.AsId(), // data_type
+		sequence.Start,             // start_value
+		sequence.Minimum,           // min_value
+		sequence.Maximum,           // max_value
+		sequence.Increment,         // increment_by
+		sequence.Cycle,             // cycle
+		sequence.Cache,             // cache_size
+		lastValue,                  // last_value
 	}, nil
 }
 
