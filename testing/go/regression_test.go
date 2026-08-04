ddc74f074596fa63444036381b1d467306edbf35
jennifersp <jennifer@dolthub.com>
2026-08-04 15:04:54 -0700

    system column used in join condition

--- testing/go/regression_test.go
+++ testing/go/regression_test.go
@@ -293,5 +293,49 @@ func TestRegressions(t *testing.T) {
 				},
 			},
 		},
+		{
+			Name: "tableoid hidden column support in join condition",
+			Assertions: []ScriptTestAssertion{
+				{
+					Query: `
+SELECT cls.oid,
+       cls.relname AS NAME,
+       CASE contype
+         WHEN 'p'
+         THEN desp.description
+         WHEN 'u'
+         THEN desp.description
+         WHEN 'x'
+         THEN desp.description
+         ELSE des.description
+       END         AS COMMENT
+FROM   pg_catalog.pg_index idx
+       JOIN pg_catalog.pg_class cls
+       ON cls.oid = indexrelid
+       LEFT JOIN pg_catalog.pg_depend dep
+       ON (dep.classid = cls.tableoid
+           AND dep.objid = cls.oid
+           AND dep.refobjsubid = '0'
+           AND dep.refclassid = (SELECT oid
+                                 FROM   pg_catalog.pg_class
+                                 WHERE  relname = 'pg_constraint')
+           AND dep.deptype = 'i')
+       LEFT OUTER JOIN pg_catalog.pg_constraint con
+       ON (con.tableoid = dep.refclassid
+           AND con.oid = dep.refobjid)
+       LEFT OUTER JOIN pg_catalog.pg_description des
+       ON (des.objoid = cls.oid
+           AND des.classoid = 'pg_class'::REGCLASS)
+       LEFT OUTER JOIN pg_catalog.pg_description desp
+       ON (desp.objoid = con.oid
+           AND desp.objsubid = 0
+           AND desp.classoid = 'pg_constraint'::REGCLASS)
+WHERE  indrelid = 1397286223::OID
+       AND contype = 'p'
+`,
+					Expected: []sql.Row{},
+				},
+			},
+		},
 	})
 }
