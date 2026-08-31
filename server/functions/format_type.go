e002244af44cec4054609a5e2b3430b04e624564
Jason Fulghum <jason@dolthub.com>
2026-08-31 11:03:17 -0700

    Expose vector type modifiers in catalogs

--- server/functions/format_type.go
+++ server/functions/format_type.go
@@ -15,11 +15,16 @@
 package functions
 
 import (
+	"bytes"
+	"fmt"
+
 	"github.com/dolthub/go-mysql-server/sql"
 	"github.com/lib/pq/oid"
 
+	"github.com/dolthub/doltgresql/core"
 	"github.com/dolthub/doltgresql/core/id"
 
+	"github.com/dolthub/doltgresql/postgres/parser/lex"
 	"github.com/dolthub/doltgresql/postgres/parser/types"
 	"github.com/dolthub/doltgresql/server/functions/framework"
 	pgtypes "github.com/dolthub/doltgresql/server/types"
@@ -47,6 +52,74 @@ var format_type = framework.Function2{
 				return t.SQLStandardNameWithTypmod(true, int(val2.(int32))), nil
 			}
 		}
-		return "???", nil
+		typ, err := getDoltgresTypeFromId(ctx, val1.(id.Id))
+		if err != nil {
+			if pgtypes.ErrTypeDoesNotExist.Is(err) {
+				return "???", nil
+			}
+			return nil, err
+		}
+		return formatUserDefinedType(ctx, typ, val2)
 	},
+}
+
+// formatUserDefinedType renders a catalog type using PostgreSQL's generic type-name and typmod rules.
+func formatUserDefinedType(ctx *sql.Context, typ *pgtypes.DoltgresType, typmodValue any) (string, error) {
+	isArray := typ.IsArrayType()
+	if isArray {
+		typ = typ.ArrayBaseType()
+	}
+
+	name, err := visibleTypeName(ctx, typ.ID)
+	if err != nil {
+		return "", err
+	}
+	if typmodValue != nil && typmodValue.(int32) >= 0 {
+		typmod := typmodValue.(int32)
+		if typ.ModOutFunc == 0 {
+			name = fmt.Sprintf("%s(%d)", name, typmod)
+		} else {
+			modifier, err := typ.TypModOut(ctx, typmod)
+			if err != nil {
+				return "", err
+			}
+			name += modifier
+		}
+	}
+	if isArray {
+		name += "[]"
+	}
+	return name, nil
+}
+
+// visibleTypeName returns a quoted type name, schema-qualifying it when an unqualified reference would resolve elsewhere.
+func visibleTypeName(ctx *sql.Context, typID id.Type) (string, error) {
+	typCol, err := core.GetTypesCollectionFromContext(ctx, "")
+	if err != nil {
+		return "", err
+	}
+	searchPath, err := core.SearchPath(ctx)
+	if err != nil {
+		return "", err
+	}
+	for _, schema := range searchPath {
+		candidate, err := typCol.GetType(ctx, id.NewType(schema, typID.TypeName()))
+		if err != nil {
+			return "", err
+		}
+		if candidate != nil {
+			if candidate.ID == typID {
+				return quoteTypeIdentifier(typID.TypeName()), nil
+			}
+			break
+		}
+	}
+	return quoteTypeIdentifier(typID.SchemaName()) + "." + quoteTypeIdentifier(typID.TypeName()), nil
+}
+
+// quoteTypeIdentifier quotes a type or schema name when PostgreSQL would not accept it as a bare identifier.
+func quoteTypeIdentifier(name string) string {
+	var buf bytes.Buffer
+	lex.EncodeRestrictedSQLIdent(&buf, name, 0)
+	return buf.String()
 }
