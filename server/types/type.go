ab5d6de9b17e7a5768b83491853d9680686e1a50
Daylon Wilkins <daylon@liquidata.co>
2026-09-15 22:17:14 -0700

    Fixes #3334

--- server/types/type.go
+++ server/types/type.go
@@ -567,7 +567,7 @@ func (t *DoltgresType) Convert(ctx context.Context, v interface{}) (interface{},
 		if ok {
 			return v, sql.InRange, nil
 		}
-	case "oid", "regclass", "regproc", "regtype":
+	case "oid", "regclass", "regnamespace", "regproc", "regtype":
 		if _, ok := v.(id.Id); ok {
 			return v, sql.InRange, nil
 		}
@@ -1170,7 +1170,7 @@ func (t *DoltgresType) Type() query.Type {
 			return sqltypes.Decimal
 		case "oid":
 			return sqltypes.VarChar
-		case "regclass", "regproc", "regtype":
+		case "regclass", "regnamespace", "regproc", "regtype":
 			return sqltypes.Text
 		default:
 			// TODO
@@ -1264,7 +1264,7 @@ func (t *DoltgresType) Zero() interface{} {
 			return int64(0)
 		case "numeric":
 			return apd.New(0, 0)
-		case "oid", "regclass", "regproc", "regtype":
+		case "oid", "regclass", "regnamespace", "regproc", "regtype":
 			return id.Null
 		default:
 			// TODO
