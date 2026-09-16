824b2f9fdcca2ffd9deb62a48d7358f724d2f279
Daylon Wilkins <daylon@liquidata.co>
2026-09-15 22:18:11 -0700

    Fixes #3332

--- server/ast/table_def.go
+++ server/ast/table_def.go
@@ -61,7 +61,7 @@ func assignTableDef(ctx *Context, node tree.TableDef, target *vitess.DDL) error 
 				return err
 			}
 			target.TableSpec.Indexes = append(target.TableSpec.Indexes, &vitess.IndexDefinition{
-				Info:   &vitess.IndexInfo{Unique: true},
+				Info:   &vitess.IndexInfo{Name: vitess.NewColIdent(string(node.UniqueConstraintName)), Unique: true},
 				Fields: indexFields,
 			})
 		}
